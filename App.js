6cfcfc0646bb0e37a295b6dafb43691937a181bb
Tạ Quang Toàn <toanta@authenticeducationhub.com>
2024-02-19 16:38:07 +0700

    test native app

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image, ScrollView, TextInput } from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  console.log(text);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.view}>
+        <Text style={styles.text}>Test app</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View>
+        <TextInput onChangeText={(e) => setText(e)} placeholder="Test Text 4" style={styles.textInput} />
+      </View>
+      <Image style={styles.image} source={{ uri: "https://i.ytimg.com/vi/vyL9iQbcAD8/maxresdefault.jpg" }} />
+      <Image style={styles.image} source={{ uri: "https://i.ytimg.com/vi/vyL9iQbcAD8/maxresdefault.jpg" }} />
+      <Image style={styles.image} source={{ uri: "https://i.ytimg.com/vi/vyL9iQbcAD8/maxresdefault.jpg" }} />
+      <Image style={styles.image} source={{ uri: "https://i.ytimg.com/vi/vyL9iQbcAD8/maxresdefault.jpg" }} />
+      <Image style={styles.image} source={{ uri: "https://i.ytimg.com/vi/vyL9iQbcAD8/maxresdefault.jpg" }} />
+      <View style={styles.view}>
+        <Text style={styles.text}>Test app</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.view}>
+        <Text style={styles.text}>Test app</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.view}>
+        <Text style={styles.text}>Test app</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.view}>
+        <Text style={styles.text}>Test app</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.view}>
+        <Text style={styles.text}>Test app</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    flexWrap: "wrap",
+  },
+  view: {
+    width: "33%",
+  },
+  text: {
+    fontSize: 30,
+    color: "red",
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  textInput: {
+    padding: 16,
+    fontSize: 16,
   },
 });
